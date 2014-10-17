957df44218efcccbb9f5c3ead6398d427fce7036
bobpal <bobpal27@yahoo.com>
2014-10-16 23:04:00 -0500

    Added icon

--- BobbyFischer/BobbyFischer/Properties/Resources.Designer.cs
+++ BobbyFischer/BobbyFischer/Properties/Resources.Designer.cs
@@ -901,6 +901,16 @@ namespace BobbyFischer.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon palagano {
+            get {
+                object obj = ResourceManager.GetObject("palagano", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap possible {
