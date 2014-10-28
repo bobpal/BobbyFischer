89a68cd2ba6bf43108820f18d8c91c5ca58bdcd9
bobpal <bobpal27@yahoo.com>
2014-10-27 21:14:00 -0500

    redundant icon

--- BobbyFischer/BobbyFischer/Properties/Resources.Designer.cs
+++ BobbyFischer/BobbyFischer/Properties/Resources.Designer.cs
@@ -911,16 +911,6 @@ namespace BobbyFischer.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
-        /// </summary>
-        internal static System.Drawing.Icon palagano {
-            get {
-                object obj = ResourceManager.GetObject("palagano", resourceCulture);
-                return ((System.Drawing.Icon)(obj));
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap possible {
